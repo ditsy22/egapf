ad7d1efe977df315974dbe9474e752d3c9246918
ditsy22 <167665674+ditsy22@users.noreply.github.com>
2024-04-21 11:31:19 +0700

    first commit

---
+++ application/views/home/kontak.php
@@ -0,0 +1,75 @@
+<section class="breadcrumb-section section-b-space">
+	<ul class="circles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h3>Kontak</h3>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Breadcrumb section end -->
+
+<!-- Contact Section Start -->
+<section class="contact-section">
+	<div class="container">
+		<div class="row g-4">
+			<div class="col-lg-7">
+				<div class="materialContainer">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.6650492584145!2d110.31678641432799!3d-7.9300084811602645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7aff9b29f1cc95%3A0xa36dfc93078c9ac5!2sGanjuran%20Indah%20Inn!5e0!3m2!1sid!2sid!4v1675065971342!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+				</div>
+			</div>
+
+			<div class="col-lg-5">
+				<div class="contact-details">
+					<div>
+						<h2>Menjadi lebih dekat</h2>
+						<h5 class="font-light">Kami selalu ada untuk segala pertanyaan</h5>
+						<div class="contact-box">
+							<div class="contact-icon">
+								<i data-feather="map-pin"></i>
+							</div>
+							<div class="contact-title">
+								<h4>Alamat :</h4>
+								<p>Ganjuran Indah, Gedogan RT 08, Sumbermulyo, Bambanglipuro, Bantul</p>
+							</div>
+						</div>
+
+						<div class="contact-box">
+							<div class="contact-icon">
+								<i data-feather="phone"></i>
+							</div>
+							<div class="contact-title">
+								<h4>No. Telepon :</h4>
+								<p>0858 7578 9647</p>
+							</div>
+						</div>
+
+						<div class="contact-box">
+							<div class="contact-icon">
+								<i data-feather="mail"></i>
+							</div>
+							<div class="contact-title">
+								<h4>Alamat Email :</h4>
+								<p>deninurhartanto27@gmail.com</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<br><br>
